fb88ee903124701759ba9b70a3a8029ed96fa8a7
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2023-03-04 12:26:09 +0530

    video bg

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ function App() {
 
   return (
     <div className="App">
-      <img src="images/brand.png" />
+      <video autoPlay muted loop class="background-video">
+        <source src="images/rain_vid.mp4" type="video/mp4" />
+      </video>
+      {/* <img src="images/Artyfycial_black.png" /> */}
       <h2>Coming soon.</h2>
     </div>
   )
